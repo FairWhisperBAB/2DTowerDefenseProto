9496df2c98a1d9eb3e1fa682f805324d34a8d33d
Benjamin Briggs <bbriggs2002@gmail.com>
2023-04-06 13:58:38 -0500

    made an APK and put it on a phone

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using static UnityEngine.GraphicsBuffer;
